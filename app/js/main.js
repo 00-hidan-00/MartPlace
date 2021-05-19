617314226e5ac95694c16fa8e42ab13e7365d4e9
00-hidan-00 <david.taranov@gmail.com>
2021-05-19 23:54:40 +0300

    end index

--- app/js/main.js
+++ app/js/main.js
@@ -16,5 +16,16 @@ $(function () {
       prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
       nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>'
    });
+
+
+
+   $('.followers-slider__inner').slick({
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: '<button class="slick-arrow slick-prev blue"><span class="lnr lnr-chevron-left"></span></button>',
+      nextArrow: '<button class="slick-arrow slick-next blue"><span class="lnr lnr-chevron-right"></span></button>'
+   });
 });
 
