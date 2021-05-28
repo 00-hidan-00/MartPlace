e9af7c04aa3bcf3f1f8cca598bd90e3768685afa
00-hidan-00 <david.taranov@gmail.com>
2021-05-28 21:50:33 +0300

    added product-one

--- app/js/main.js
+++ app/js/main.js
@@ -33,6 +33,17 @@ $(function () {
 
 });
 
+
+
+$('.wrapper .tab').on('click', function (event) {
+   var id = $(this).attr('data-id');
+   $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
+   $('.wrapper .tabs').find('.tab').removeClass('active');
+   $(this).addClass('active');
+   $('#' + id).addClass('active-tab').fadeIn();
+   return false;
+});
+
 $('.menu__btn').on('click', function () {
    $('.menu__list').slideToggle();
 });
